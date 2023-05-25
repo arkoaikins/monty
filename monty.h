8a6c05568193effc3f15b773747685a55db5208e
arkoaikins <arkoaikins@gmail.com>
2023-05-25 22:13:53 +0000

    header file to be updated

---
+++ monty.h
@@ -0,0 +1,58 @@
+#ifndef MONTY_H
+#define MONTY_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <ctype.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+
+#define _GNU_SOURCE
+#define _POSIX_C_SOURCE 200809L
+
+/**
+ * struct stack_s - doubly linked list representation of a stack (or queue)
+ * @n: integer
+ * @prev: points to the previous element of the stack (or queue)
+ * @next: points to the next element of the stack (or queue)
+ *
+ * Description: doubly linked list node structure
+ * for stack, queues, LIFO, FIFO
+ */
+typedef struct stack_s
+{
+	int n;
+	struct stack_s *prev;
+	struct stack_s *next;
+} stack_t;
+
+/**
+ * struct instruction_s - opcode and its function
+ * @opcode: the opcode
+ * @f: function to handle the opcode
+ *
+ * Description: opcode and its function
+ * for stack, queues, LIFO, FIFO
+ */
+typedef struct instruction_s
+{
+	char *opcode;
+	void (*f)(stack_t **stack, unsigned int line_number);
+} instruction_t;
+
+/*check if it is a number*/
+int ifnumber(char *s);
+
+/*prototypes of functions*/
+void pall(stack_t **stack, unsigned int l_numb);
+void push(stack_t **stack, unsigned int l_numb);
+void pop(stack_t **stack, unsigned int l_numb);
+void pint(stack_t **stack, unsigned int l_numb);
+void swap(stack_t **stack, unsigned int l_numb);
+void add(stack_t **stack, unsigned int l_numb);
+
+#endif
