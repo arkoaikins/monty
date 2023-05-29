17613238ed1e637d9175f5684cf73e980c5b3b4f
arkoaikins <arkoaikins@gmail.com>
2023-05-29 00:20:42 +0000

    update pint_pop.c implementation

--- pint_pop.c
+++ pint_pop.c
@@ -1,18 +1,16 @@
 #include "monty.h"
-
 /**
  * pint - The opcode pint prints the value at the top of the stack
  *        followed by a new line.
  * @stack: points to the stack
- * @l_numb: line number of bytecode file
+ * @l_numb: number given to the element of the stack
  * Return: it returns nothing(void)
- */
-
-void pint(stack_t **stack, unsigned int l_numb)
+*/
+void pint(stack_t **stack, unsigned int  l_numb)
 {
-	if (*stack == NULL)
+	if (!(*stack))
 	{
-		dprintf(2, "L%u: can't pint, stack empty\n", l_numb);
+		dprintf(2, "L%u: can't pint, stack empty\n",  l_numb);
 		exit(EXIT_FAILURE);
 	}
 	printf("%d\n", (*stack)->n);
@@ -20,17 +18,17 @@ void pint(stack_t **stack, unsigned int l_numb)
 
 /**
  * pop - The opcode pop removes the top element of the stack.
- * @stack: points to the stack
- * @l_numb: line number of bytecode file
+ * @stack: points to stack
+ * @l_numb: number given to the element of the stack
  * Return:It returns nothing(void)
 */
-void pop(stack_t **stack, unsigned int l_numb)
+void pop(stack_t **stack, unsigned int  l_numb)
 {
 	stack_t *tmp;
 
-	if (*stack == NULL)
+	if (!(*stack))
 	{
-		dprintf(2, "L%u: can't pop an empty stack\n", l_numb);
+		dprintf(2, "L%u: can't pop an empty stack\n",  l_numb);
 		exit(EXIT_FAILURE);
 	}
 	tmp = *stack;
