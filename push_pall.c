82f0fe7348184d6c730bb6964d69eebf9c04fa09
arkoaikins <arkoaikins@gmail.com>
2023-05-29 00:13:25 +0000

    update push and pull implementation

--- push_pall.c
+++ push_pall.c
@@ -1,5 +1,4 @@
 #include "monty.h"
-#include <ctype.h>
 
 int ifnumber(char *s);
 
@@ -7,57 +6,52 @@ int ifnumber(char *s);
  * push - the opcode push pushes an element to the stack
  * @stack: points to the stack
  * @l_numb: number given to the element of the stack
- * Return: it returns nothing (void)
- */
-void push(stack_t **stack, unsigned int l_numb)
+ *  Return: it returns nothing (void)
+*/
+void push(stack_t **stack, unsigned int  l_numb)
 {
-	char *input;
-	int not_int;
 	stack_t *node1;
+	int status;
 
-	node1 = malloc(sizeof(stack_t));
+	node1 = malloc(sizeof(size_t));
 	if (!node1)
 	{
-		dprintf(STDERR_FILENO, "Error: malloc failed\n");
+		dprintf(2, "Error: malloc failed\n");
 		exit(EXIT_FAILURE);
 	}
 
-	input = strtok(NULL, " ");
-	not_int = ifnumber(input);
-
-	if (not_int == -1)
+	if ((ifnumber(monty.arg) == -1) || !monty.arg)
 	{
-		dprintf(STDERR_FILENO, "L%u: usage: push integer\n", l_numb);
+		dprintf(2, "L%u: usage: push integer\n",  l_numb);
 		exit(EXIT_FAILURE);
 	}
-
-	node1->n = atoi(input);
+	status = atoi(monty.arg);
+	node1->n = status;
 	node1->next = NULL;
 	node1->prev = NULL;
 
-	if (*stack == NULL)
+	if (!(*stack))
 	{
-		*stack = node1;
+		(*stack) = node1;
+		return;
 	}
-	else
-	{
-		node1->next = *stack;
-		(*stack)->prev = node1;
-		*stack = node1;
-	}
+	node1->next = (*stack);
+	(*stack)->prev = node1;
+	(*stack) = node1;
 }
 
 /**
  * pall - prints all the values on the stack, starting from the top
  * @stack: points to the stack
- * @l_numb: number given to the element of the stack
+ * @l_numb:number given to the element of the stack
  * Return: it returns nothing (void)
- */
-void pall(stack_t **stack, unsigned int l_numb)
+*/
+void pall(stack_t **stack, unsigned int  l_numb)
 {
-	(void)l_numb;
-	stack_t *tmp = *stack;
+	stack_t *tmp;
+	(void) l_numb;
 
+	tmp = *stack;
 	while (tmp)
 	{
 		printf("%d\n", tmp->n);
@@ -65,21 +59,21 @@ void pall(stack_t **stack, unsigned int l_numb)
 	}
 }
 
+
 /**
  * ifnumber - check for number
  * @s: the string to check for a number
  * Return: 1 if it is a number, else -1
- */
+*/
 int ifnumber(char *s)
 {
 	int i = 0;
 
 	while (s[i])
 	{
-		if (!isdigit(s[i]))
+		if (!(isdigit(s[i])))
 			return (-1);
 		i++;
 	}
 	return (1);
 }
-
