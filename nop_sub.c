d75bb0e604242c8fd3a3f7be28b133805c7690be
arkoaikins <arkoaikins@gmail.com>
2023-05-29 00:34:34 +0000

    add nob implementation

---
+++ nop_sub.c
@@ -0,0 +1,13 @@
+#include "monty.h"
+
+/**
+ * nop - The opcode nop doesn’t do anything.
+ * @stack: points to the stack
+ * @l_numb: number given to the element of the stack
+ * Return: it returns nothing (void)
+ */
+void nop(stack_t **stack, unsigned int l_numb)
+{
+	(void)stack;
+	(void)l_numb;
+}
